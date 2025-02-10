d7219b11f64c9560d34123d029fcf92bfdffa844
Dhaerya <dhaeryamore@gmail.com>
2025-02-10 15:00:42 +0530

    Abstract Method concept

---
+++ Shape.java
@@ -0,0 +1,25 @@
+//Shape.java
+
+abstract class Shape{
+	protected double dim_one, dim_two, dim_three;
+	int num_sides;
+	//Zero parameter constructor
+	public Shape(){
+		dim_one = dim_two = dim_three = 0;
+		num_sides = 0;
+	}
+
+	//Parameterized Constructor
+	public Shape(double dim_one, double dim_two, doule dim_three, int num_sides){
+		this.dim_one = dim_one;
+		this.dim_two = dim_two;
+		this.dim_three = dim_three;
+		this.num_sides = num_sides;
+	}	
+
+	abstract double calculateArea(); //Abstract Method
+	abstract double calculatePerimeter(); //Abstract Method
+
+	public int getNumSlides(){
+	}
+}
