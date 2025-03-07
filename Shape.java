9bcc5e04b7e09dc7808edb14577321b1b0559070
Dhaerya <dhaeryamore@gmail.com>
2025-03-07 23:23:49 +0530

    With Changes

--- Shape.java
+++ Shape.java
@@ -1,26 +1,14 @@
 // Shape.java
+abstract class Shape {
+    protected double dim_one, dim_two, dim_three;
+    int num_sides;
+    String shapeName;
 
-abstract class Shape{
-	protected double dim_one, dim_two, dim_three;
-	int num_sides;
-	
-	// Zero parameter constructor
-	public Shape(){
-		dim_one = dim_two = dim_three = 0;
-		num_sides = 0;
-	}
+    // Constructor with shape name
+    public Shape(String shapeName) {
+        this.shapeName = shapeName;
+    }
 
-	//Parameter constructor
-	public Shape(double dim_one, double dim_two, double dim_three, int num_sides){
-		this.dim_one = dim_one;
-		this.dim_two = dim_two;
-		this.dim_three = dim_three;
-		this.num_sides = num_sides;
-	}
-	abstract double calculateArea(); //Abstract Method
-	abstract double calculatePerimeter(); //Abstract Method
-	
-	public int getNumSides(){
-		return num_sides;
-	}
+    abstract double calculateArea();
+    abstract double calculatePerimeter();
 }
